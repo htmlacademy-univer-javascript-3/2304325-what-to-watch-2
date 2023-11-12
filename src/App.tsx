9bb2d7530408e5dbfdf8566bb5820b87a10f753d
Alexandr Kozlov <alexandr.kozlov@slife.team>
2023-11-12 21:36:45 +0300

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,17 @@
-import MainPage from './MainPage';
+import MainPage from './main-page';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import SignInPAge from './SignInPage';
+import MyListPage from './my-list-page';
+import FilmPage from './film-page';
+import AddReviewPage from './add-review-page';
+import PlayerPage from './player-page';
+import NotFoundPage from './not-found-page';
+import PrivateRoute from './components/private-route';
+
 
 type Props = {
   name: string;
@@ -7,8 +20,40 @@ type Props = {
 }
 
 const App = (props : Props) => (
-
-  <MainPage {...props}/>
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path='/'
+        element={<MainPage {...props}/>}
+      />
+      <Route
+        path='/login'
+        element={<SignInPAge/>}
+      />
+      <Route path='/mylist' element={
+        <PrivateRoute>
+          <MyListPage/>
+        </PrivateRoute>
+      }
+      />
+      <Route
+        path='/films/:id'
+        element={<FilmPage/>}
+      />
+      <Route
+        path='/films/:id/review'
+        element={<AddReviewPage/>}
+      />
+      <Route
+        path='/Player '
+        element={<PlayerPage/>}
+      />
+      <Route
+        path='*'
+        element={<NotFoundPage/>}
+      />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/not-found-page.tsx
@@ -0,0 +1,7 @@
+const NotFoundPage = () => (
+  <div>
+    <h2>Not found page</h2>
+  </div>
+);
+
+export default NotFoundPage;
