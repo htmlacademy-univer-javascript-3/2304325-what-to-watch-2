49fbdbbd6c3fe9d418fec0883a6b2f96ce79e1d5
Alexandr Kozlov <alexandr.kozlov@slife.team>
2023-12-12 13:58:56 +0300

    module6-task2

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,11 +1,14 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { changeGenre, getFilmsByGenre } from './action.ts';
+import { changeGenre, getFilmsByGenre, showMoreFilms } from './action.ts';
 import { catalogFilmCards } from '../mocks/films.ts';
 import { GENRE_ALL_GENRES } from '../const/const.ts';
 
 const initialState = {
-  genre: 'All genres',
   films: catalogFilmCards,
+  counter: 8,
+  genre: GENRE_ALL_GENRES,
+  currentFilms: catalogFilmCards.slice(0,8),
+  currentFilmsLength: catalogFilmCards.length
 };
 
 const reducer = createReducer(initialState, (builder) => {
@@ -16,9 +19,17 @@ const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(getFilmsByGenre, (state, action) => {
       const {genre} = action.payload;
-      state.films =
+      state.counter = 8;
+      state.currentFilmsLength = catalogFilmCards.filter((item) => genre === GENRE_ALL_GENRES ? item : item.genre === genre).length;
+      state.currentFilms =
       catalogFilmCards
-        .filter((item) => genre === GENRE_ALL_GENRES ? item : item.genre === genre);
+        .filter((item) => genre === GENRE_ALL_GENRES ? item : item.genre === genre).filter((_, index) => index < state.counter);
+    })
+    .addCase(showMoreFilms, (state) => {
+      state.counter = state.counter + 8;
+      state.currentFilms =
+      catalogFilmCards
+        .filter((item) => state.genre === GENRE_ALL_GENRES ? item : item.genre === state.genre).filter((_, index) => index < state.counter);
     });
 });
 
