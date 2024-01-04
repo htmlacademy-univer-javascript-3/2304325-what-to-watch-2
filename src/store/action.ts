bcb4176bfcdc05171b868f74d5764e84d11e4b86
Alexandr Kozlov <alexandr.kozlov@slife.team>
2024-01-04 17:45:21 +0300

    module9-task1

--- src/store/action.ts
+++ src/store/action.ts
@@ -3,8 +3,11 @@ import { FilmsPreviewData, User } from '../types/types';
 import { AppRoute, AuthStatus } from '../const/const';
 
 export const changeGenre = createAction<{genre: string}>('genre/changeGenre');
+
 export const getFilmsByGenre = createAction<{genre: string}>('films/getFilmsByGenre');
+
 export const showMoreFilms = createAction('films/showMoreFilms');
+
 export const resetCurrentFilms = createAction('films/resetCurrentFilms');
 
 export const loadFilms = createAction<FilmsPreviewData>('films/loadFilmsData');
